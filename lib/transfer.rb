76e8cb8ca7881a164c8f372486fe7b6ad84adaad
YUKIKO IKEDA <applelele@gmail.com>
2020-06-22 23:27:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,8 +24,8 @@ class Transfer
       @receiver.balance += amount
       @status = "complete"
     else
-      return "Transaction rejected. Please check your account balance."
       @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
     end
   end
 
