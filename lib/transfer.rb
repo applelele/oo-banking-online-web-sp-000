e4a1f84ca42716faabe392913770e25e208af824
YUKIKO IKEDA <applelele@gmail.com>
2020-06-22 23:00:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
+
+  def initialize(sender, receiver, status = "pending", amount)
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+  end
   # your code here
 end
